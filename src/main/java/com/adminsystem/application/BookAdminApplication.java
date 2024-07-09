edd81a37bf5792b14a4cb7528305c89d555d551f
usename6 <821101242@qq.com>
2024-07-09 12:51:41 +0800

    feat:创建第一个springboot的项目

---
+++ src/main/java/com/adminsystem/application/BookAdminApplication.java
@@ -0,0 +1,10 @@
+package com.adminsystem.application;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookAdminApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookAdminApplication.class,args);
+    }
+}
